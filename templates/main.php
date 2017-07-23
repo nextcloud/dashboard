a70650e72b767102b4af890eb48649c73bb9591b
Maxence Lange <maxence@nextcloud.com>
2017-07-23 20:32:50 +0200

    4.0.1

--- templates/main.php
+++ templates/main.php
@@ -1,8 +1,29 @@
-<input type="hidden" name="nextNonce" id="nextNonce" value="<?php p(
-	\OC::$server->getContentSecurityPolicyNonceManager()
-				->getNonce()
-) ?>"/>
 <?php
+/**
+ * Nextcloud - Dashboard App
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ * @author regio iT gesellschaft für informationstechnologie mbh
+ * @copyright regio iT 2017
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 
 // tether (needed by boostrap)
 script('dashboard', '../vendor/bordercloud/tether/dist/js/tether.min');
@@ -37,6 +58,11 @@ script('dashboard', 'inbox');
 
 style('dashboard', 'style');
 ?>
+
+<input type="hidden" name="nextNonce" id="nextNonce" value="<?php p(
+	\OC::$server->getContentSecurityPolicyNonceManager()
+				->getNonce()
+) ?>"/>
 
 <div id="app">
 	<?php /* ?><div id="app-navigation">
