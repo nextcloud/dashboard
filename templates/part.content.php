62624daaa3b423ab207291925e5ab9f2018ee173
tuxedo-rb <37629910+tuxedo-rb@users.noreply.github.com>
2018-03-22 11:59:09 +0100

    Update part.content.php
    fix storage bar

--- templates/part.content.php
+++ templates/part.content.php
@@ -8,6 +8,7 @@
  * @author regio iT gesellschaft für informationstechnologie mbh
  * @copyright regio iT 2017
  * @license GNU AGPL version 3 or any later version
+ * @contributor tuxedo-rb | TUXEDO Computers GmbH | https://www.tuxedocomputers.com
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -41,10 +42,11 @@
         <h1><a data-toggle="myCollapse" data-target="#quota"><?php p($l->t('Used space')); ?></a></h1>
 
         <div id="quota" class="myCollapse in">
-            <div style="width: <?php p($_['storage_info']['relative']); ?>%">
-                <p id="quotatext"><?php print_unescaped($l->t('You have used <strong>%s</strong> of the available <strong>%s</strong>', $_['storage_info']['quota'])); ?></p>
-            </div>
+            <div id="quota-used" style="width: <?php p($_['storage_info']['relative']); ?>%"></div>
+            <div id="quota-limit"></div>
+            <p id="quota-text"><?php print_unescaped($l->t('You have used <strong>%s</strong> of the available <strong>%s</strong>', $_['storage_info']['quota'])); ?></p>
         </div>
+        <div style="clear: both;"></div>
     </section>
 </div>
 
