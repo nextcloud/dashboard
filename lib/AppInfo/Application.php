ae26c9a73a661418a652e6f24992537bf3b37aab
tuxedo-rb <rb@tuxedocomputers.com>
2018-05-08 10:11:28 +0200

    remove unneeded activity-app includes

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -28,89 +28,64 @@
 
 namespace OCA\Dashboard\AppInfo;
 
-use OC\Files\View;
-use OCA\Activity\Controller\Activities;
-use OCA\Activity\Controller\APIv1;
-use OCA\Activity\Controller\Feed;
-use OCA\Activity\Controller\Settings;
-use OCA\Activity\ViewInfoCache;
 use OCA\Dashboard\Controller\AdminController;
 use OCA\DAV\Connector\Sabre\Principal;
 use OCP\AppFramework\App;
 use OCP\AppFramework\IAppContainer;
 
 class Application extends App {
+
 	public function __construct(array $urlParams = array()) {
 		parent::__construct('dashboard', $urlParams);
 		$container = $this->getContainer();
 
 		// Aliases for the controllers so we can use the automatic DI
-		$container->registerAlias('OCA\Activity\ActivitiesController', Activities::class);
-		$container->registerAlias('OCA\Activity\APIv1Controller', APIv1::class);
-		$container->registerAlias('OCA\Activity\FeedController', Feed::class);
-		$container->registerAlias('OCA\Activity\SettingsController', Settings::class);
 		$container->registerAlias(
-			'AdminController', AdminController::class
-		);
-
-		$container->registerService(
-			'OCA\Activity\ViewInfoCache', function(IAppContainer $c) {
-			return new ViewInfoCache(
-				new View('')
-			);
-		}
+			'AdminController',
+			AdminController::class
 		);
 		$container->registerService(
-			'OCA\Activity\isCLI', function() {
-			return \OC::$CLI;
-		}
-		);
-		$container->registerService(
-			'OCP\IUserManager', function(IAppContainer $c) {
-			return $c->getServer()
-					 ->getUserManager();
-		}
+			'OCP\IUserManager',
+			function(IAppContainer $c) {
+				return $c->getServer()->getUserManager();
+			}
 		);
 		$container->registerService(
 			'Symfony\Component\EventDispatcher\EventDispatcherInterface',
 			function(IAppContainer $c) {
-				return $c->getServer()
-						 ->getEventDispatcher();
+				return $c->getServer()->getEventDispatcher();
 			}
 		);
 		$container->registerService(
-			'OCP\Security\ISecureRandom', function(IAppContainer $c) {
-			return $c->getServer()
-					 ->getSecureRandom();
-		}
+			'OCP\Security\ISecureRandom',
+			function(IAppContainer $c) {
+				return $c->getServer()->getSecureRandom();
+			}
 		);
 		$container->registerService(
-			'CurrentUID', function(IAppContainer $c) {
-			$user = $c->getServer()
-					  ->getUserSession()
-					  ->getUser();
-
-			return ($user) ? $user->getUID() : '';
-		}
+			'CurrentUID',
+			function(IAppContainer $c) {
+				$user = $c->getServer()->getUserSession()->getUser();
+				return ($user) ? $user->getUID() : '';
+			}
 		);
 		$container->registerService(
-			'OCP\IDBConnection', function(IAppContainer $c) {
-			return $c->getServer()
-					 ->getDatabaseConnection();
-		}
+			'OCP\IDBConnection',
+			function(IAppContainer $c) {
+				return $c->getServer()->getDatabaseConnection();
+			}
 		);
 		$container->registerService(
-			'OCA\DAV\Connector\Sabre\Principal', function(IAppContainer $c) {
-			return new Principal(
-				$c->getServer()
-				  ->getUserManager(),
-				$c->getServer()
-				  ->getGroupManager(),
-				$c->getServer()->getShareManager(),
-				$c->getServer()->getUserSession()
-			);
-		}
+			'OCA\DAV\Connector\Sabre\Principal',
+			function(IAppContainer $c) {
+				return new Principal(
+					$c->getServer()->getUserManager(),
+					$c->getServer()->getGroupManager(),
+					$c->getServer()->getShareManager(),
+					$c->getServer()->getUserSession()
+				);
+			}
 		);
-
 	}
+
 }
